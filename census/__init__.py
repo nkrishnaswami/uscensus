a365ba78a0f687a519b3b4103c3291642d26acb4
Natarajan Krishnaswami <natarajan@krishnaswami.org>
2017-04-01 17:12:56 -0400

    add module docstring

--- census/__init__.py
+++ census/__init__.py
@@ -3,3 +3,9 @@ from census.mongocache import MongoCache
 from census.dbapicache import DBAPICache
 from census.nopcache import NopCache
 from census.loader import *
+
+"""This module reads the Census's API discovery interface at
+http://api.census.gov/data.json, and provides callable wrappers for
+each API it finds.  It indexes each of their metadata fields to make
+the APIs and variables related to them easier to find.
+"""
