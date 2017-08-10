2a98e2f29151b87c0e906f145d366b1e8be1f8c0
Natarajan Krishnaswami <natarajan@krishnaswami.org>
2017-08-10 19:12:31 -0400

    export tests

---
+++ uscensus/tests/__init__.py
@@ -0,0 +1,11 @@
+from .dbapicache import DBAPICache_test
+from .dbapiqueryhelper import DBAPIQueryHelper_test
+from .discovery import DiscoveryInterface_test
+from .errors import CensusError_test
+from .errors import DBError_test
+from .index import Index_test
+from .model import CensusDataEndpoint_test
+from .nopcache import NopCache_test
+from .webcache import fetchjson_test
+from .webcache import condget_test
+
